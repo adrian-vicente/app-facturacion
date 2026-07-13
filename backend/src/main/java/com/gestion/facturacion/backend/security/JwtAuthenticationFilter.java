6c87a87b7d2a1ec802c22309aa253a8615794da9
Adrián Vicente Vera <adrianvicentevera04@gmail.com>
2026-07-13 18:52:27 +0200

    Modificación para implementar el inicio de sesión automático a través del uso de tokens

--- backend/src/main/java/com/gestion/facturacion/backend/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/gestion/facturacion/backend/security/JwtAuthenticationFilter.java
@@ -1,5 +1,48 @@
 package com.gestion.facturacion.backend.security;
 
-public class JwtAuthenticationFilter {
+import java.io.IOException;
 
-}
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    // Inyección de dependencias 
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
+
+    // Método que realiza el filtro sobre el token 
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        
+        // Obtener el header de la petición
+
+        // Comprobar si existe y que empieza por barer
+
+        // Extraer el token de la petición
+
+        // Obtener el email 
+
+        // Comprobar que el usuario no está ya autenticado 
+
+        // Cargar el usuario de la base de datos 
+
+        // Validar el token de la petición
+
+    }
+
+} // class
--- backend/src/main/java/com/gestion/facturacion/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/gestion/facturacion/backend/security/SecurityConfig.java
@@ -2,12 +2,16 @@ package com.gestion.facturacion.backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     
     @Bean
@@ -22,7 +26,15 @@ public class SecurityConfig {
 
         http
             .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/usuarios/crear").permitAll()
+                .anyRequest().authenticated()
+            )
+
+           .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+           );
 
         return http.build();
 
